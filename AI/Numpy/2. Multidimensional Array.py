19d9628ac5cc4cd660335c4e0acf8c780335d6b7
Aarav <athakur@dohacollege.com.qa>
2026-05-02 17:41:42 +0300

    Finished Lesson 2 on multidimensional Arrays Along added comments

---
+++ AI/Numpy/2. Multidimensional Array.py
@@ -0,0 +1,24 @@
+import numpy as np
+array = np.array("A")
+print(array.ndim)
+print(array.shape)
+
+array1 = np.array(["A", "B","C"])
+print(array1.ndim)
+print(array1.shape)
+
+array2 = np.array(  [["A", "B","C"], 
+                    ["D","E","F"]])
+print(array2.ndim)
+print(array2.shape)
+
+# Arrays/Matrices require a homogenous shape. Error will occur if array is heterogenous
+main_array = np.array( [[["A", "B","C"], ["D","E","F"], ["G","H","I"]],
+                        [["J", "K","L"], ["M","N","O"], ["P","Q","R"]],
+                        [["S", "T","U"], ["V","W","X"], ["Y","Z", " "]]])
+print(main_array.ndim)
+print(main_array.shape)
+
+# Exercise 1: Make the word dog from multidimensional indexing
+word = main_array[0,1,0] + main_array[1,1,2] + main_array[0,2,0]
+print(word)
